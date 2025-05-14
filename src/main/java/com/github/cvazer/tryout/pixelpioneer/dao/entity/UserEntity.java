c2fde1be711f50b4d5cf3a778244732ce398551f
Yan Frankosvki <zhirnitoni@gmail.com>
2025-05-14 14:00:01 +0300

    User entity mapped

---
+++ src/main/java/com/github/cvazer/tryout/pixelpioneer/dao/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package com.github.cvazer.tryout.pixelpioneer.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class UserEntity {
+    private String name;
+    private LocalDate dateOfBirth;
+    private String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
