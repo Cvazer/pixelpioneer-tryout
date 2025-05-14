a01dfb4d6934d382d6f72713d14ebea65454f0f8
Yan Frankosvki <zhirnitoni@gmail.com>
2025-05-14 14:41:02 +0300

    Tiding up post Spring Boot 3 reverse migration errors

--- src/main/java/com/github/cvazer/tryout/pixelpioneer/security/SecurityConfig.java
+++ src/main/java/com/github/cvazer/tryout/pixelpioneer/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,9 +23,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+                .authorizeHttpRequests()
                     .antMatchers("/login").permitAll()
                     .antMatchers("/api/**").authenticated()
                 .and()
