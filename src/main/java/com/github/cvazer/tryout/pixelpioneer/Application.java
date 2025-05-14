efd5184d7936a79e49836c9fa4a8b66abc4151d8
Yan Frankosvki <zhirnitoni@gmail.com>
2025-05-14 16:59:42 +0300

    Forgot to enable transaction management

--- src/main/java/com/github/cvazer/tryout/pixelpioneer/Application.java
+++ src/main/java/com/github/cvazer/tryout/pixelpioneer/Application.java
@@ -2,8 +2,10 @@ package com.github.cvazer.tryout.pixelpioneer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass = true)
 public class Application {
 
     public static void main(String[] args) {
