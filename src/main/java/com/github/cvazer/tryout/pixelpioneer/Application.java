43365e5ac434e24375c1556bef42e081e8a10036
Yan Frankosvki <zhirnitoni@gmail.com>
2025-05-15 11:10:10 +0300

    Enabled spring boot caching backed by redis

--- src/main/java/com/github/cvazer/tryout/pixelpioneer/Application.java
+++ src/main/java/com/github/cvazer/tryout/pixelpioneer/Application.java
@@ -2,8 +2,10 @@ package com.github.cvazer.tryout.pixelpioneer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableCaching
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
 public class Application {
